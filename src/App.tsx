a0aedf95eff24d1d1ac23b20ca78e10cc06e13fc
Cliente CONTROL-PC <CltControl@GGIECLUGWRK165.espol.edu.ec>
2024-11-13 10:08:29 -0500

    feat: dashboard layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Grid2 as Grid} from '@mui/material'
+import Grid from '@mui/material/Grid2'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <Grid>
+    <Grid container spacing={5}>
 
     {/* Indicadores */}
-    <Grid>Elemento: Indicador 1</Grid>
-    <Grid>Elemento: Indicador 2</Grid>
-    <Grid>Elemento: Indicador 3</Grid>
-    <Grid>Elemento: Indicador 4</Grid>
+    <Grid size={{xs: 12, xl:3}}>Elemento: Indicador 1</Grid>
+    <Grid size={{xs: 12, xl:3}}>Elemento: Indicador 2</Grid>
+    <Grid size={{xs: 12, xl:3}}>Elemento: Indicador 3</Grid>
+    <Grid size={{xs: 12, xl:3}}>Elemento: Indicador 4</Grid>
    
     {/* Tabla */}
-    <Grid>Elemento: Tabla</Grid>
+    <Grid size={{xs: 12, xl:8}}>Elemento: Tabla</Grid>
    
     {/* Gráfico */}
-    <Grid>Elemento: Gráfico 1</Grid>
+    <Grid size={{xs: 12, xl:4}}>Elemento: Gráfico 1</Grid>
 
 </Grid>
   )

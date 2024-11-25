41bbe41077f2495eca64ee5d1db5a06724cf71fe
Cliente CONTROL-PC <CltControl@GGIECLUGWRK165.espol.edu.ec>
2024-11-25 09:38:37 -0500

    feat: indicator weather data

--- src/components/IndicatorWeather.tsx
+++ src/components/IndicatorWeather.tsx
@@ -1,7 +1,14 @@
+interface Indicator {
+    title?: String;
+    subtitle?: String;
+    value?: String;
+}
 
-export default function IndicatorWeather() {
-    return ( <>
-    Componente indicator weather
+export default function IndicatorWeather(config : Indicator) {
+    return (    <>
+        {config.title}<br/>
+        {config.value}<br/>
+        {config.subtitle}
     </> );
 }
 
